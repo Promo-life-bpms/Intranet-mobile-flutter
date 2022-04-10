bcda15080fb6c17bddb666bccf2f38c6590b6829
Oscar-CR <krz.oficial@gmail.com>
2022-04-10 17:30:45 -0500

    separacion de widgets y agregado de assets

--- lib/views/access/access_page.dart
+++ lib/views/access/access_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:intranet_movil/widget/navigation_drawer_widget.dart';
+import 'package:intranet_movil/widget/card_image_link.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:intranet_movil/widget/navigation_drawer_widget.dart';
+ 
 
+void main() => runApp(AccessPage());
 
-void main() => runApp(const AccessPage());
+class AccessPage extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
 
-class AccessPage extends StatelessWidget {
-  const AccessPage({Key? key}) : super(key: key);
+class _State extends State<AccessPage> {
+  final List<String> accessName = <String>['CURSOS', 'ODDO', 'EVALUACIÓN 360', 'NOM 035', 'Cotizador','Sistema de Tickets','Power BI'];
+  final List<String> accessImage = <String>['lib/assets/course.png', 'lib/assets/odoo.png','lib/assets/evaluacion.png', 'lib/assets/nom.png', 'lib/assets/cotizador.png', 'lib/assets/tickets.png','lib/assets/powerbi.png'];
+  final List<String> accessLink = <String>['https://dev-cursos.promolife.lat/loginEmail?email=admin@admin.com&password=password', 'https://promolife.vde-suite.com:8030/web/login', 'https://evaluacion.promolife.lat/login', 'https://plataforma.nom-035.net/', 'https://promolife.lat/login/?redirect_to=https%3A%2F%2Fpromolife.lat%2F','https://tdesign.promolife.lat/','https://app.powerbi.com/singleSignOn?ru=https:%2f%2fapp.powerbi.com%2f%3fnoSignUpCheck%3d1'];
 
   static const String _title = 'Accesos';
 
@@ -17,160 +25,70 @@ class AccessPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(_title),
       ),
-      body: const AccessWidget(),
-    );
-  }
-}
-
-/* Prueba git
- */
-class AccessWidget extends StatelessWidget {
-  const AccessWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child:Container(
-        margin: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-        child: Column(
-          children:<Widget> [
-            Card(
-              child: Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-                child: Column(
-                  children: <Widget> [
-                  const SizedBox(
-                    width: double.infinity,
-                    height: 160.0,
-                    child:FittedBox(
-                      fit: BoxFit.contain,
-                      child: Image(
-                          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                        )
-                    ),
-                  ),
-                  const Text(
-                    "Cursos",
-                    style: TextStyle(fontSize: 20.00,fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  ElevatedButton( 
-                  onPressed: () {
-                    _launchURL("https://www.google.com.mx");
-                  },
-                  child: const Text('INICIAR SESION'),
-                  ),
-                ],
-                ),
-              ),
-            ),
-
-            Card(
-              child: Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-                child: Column(
-                  children: <Widget> [
-                  const SizedBox(
-                    width: double.infinity,
-                    height: 160.0,
-                    child:FittedBox(
-                      fit: BoxFit.contain,
-                      child: Image(
-                          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                        )
-                    ),
-                  ),
-                  const Text(
-                    "Cursos",
-                    style: TextStyle(fontSize: 20.00,fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  ElevatedButton( 
-                  onPressed: () {
-                    _launchURL("https://www.google.com.mx");
-                  },
-                  child: const Text('INICIAR SESION'),
-                  ),
-                ],
-                ),
-              ),
-            ),
-
-            Card(
-              child: Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-                child: Column(
-                  children: <Widget> [
-                  const SizedBox(
-                    width: double.infinity,
-                    height: 160.0,
-                    child:FittedBox(
-                      fit: BoxFit.contain,
-                      child: Image(
-                          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                        )
-                    ),
-                  ),
-                  const Text(
-                    "Cursos",
-                    style: TextStyle(fontSize: 20.00,fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  ElevatedButton( 
-                  onPressed: () {
-                    _launchURL("https://www.google.com.mx");
-                  },
-                  child: const Text('INICIAR SESION'),
-                  ),
-                ],
-                ),
-              ),
-            ),
-
-            Card(
-              child: Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-                child: Column(
-                  children: <Widget> [
-                  const SizedBox(
-                    width: double.infinity,
-                    height: 160.0,
-                    child:FittedBox(
-                      fit: BoxFit.contain,
-                      child: Image(
-                          image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
-                        )
-                    ),
-                  ),
-                  const Text(
-                    "Cursos",
-                    style: TextStyle(fontSize: 20.00,fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  ElevatedButton( 
-                  onPressed: () {
-                    _launchURL("https://www.google.com.mx");
-                  },
-                  child: const Text('INICIAR SESION'),
-                  ),
-                ],
-                ),
-              ),
-            ),
-
-
-
-          ],
-        ),
+      body: CardImageLinkWidget(
+        accessData : List<AccessData>.generate(accessName.length, (index) => AccessData(accessName[index], accessImage[index],accessLink[index])),            
       ),
     );
   }
 }
 
+
 void _launchURL(_url) async {
   if (!await launch(_url)) throw 'Could not launch $_url';
 }
 
+class AccessData {
+  final String accName;
+  final String accImage;
+  final String accLink;
 
+  const AccessData(this.accName, this.accImage, this.accLink);
+}
+
+
+
+class CardImageLinkWidget  extends StatelessWidget {
+  const CardImageLinkWidget({Key? key, required this.accessData}) : super(key: key);
+
+  final List<AccessData> accessData;
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: accessData.length,
+        itemBuilder: (context, index) {
+          return Card(
+          child: Padding(
+          padding: const EdgeInsets.only(bottom: 20.0),
+            child: Column(
+              children: <Widget> [
+              SizedBox(
+                width: double.infinity,
+                height: 160.0,
+                child:FittedBox(
+                  fit: BoxFit.contain,
+                  child: Image(
+                      image: AssetImage(accessData[index].accImage),
+                    )
+                ),
+              ),
+                Text(
+                accessData[index].accName,
+                style: const TextStyle(fontSize: 20.00,fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton( 
+              onPressed: () {
+                _launchURL(accessData[index].accLink); 
+              },
+              child: const Text('ABRIR'),
+              ),
+            ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
 
--- lib/widget/navigation_drawer_widget.dart
+++ lib/widget/navigation_drawer_widget.dart
@@ -170,7 +170,7 @@ class NavigationDrawerWidget extends StatelessWidget {
         break;
       case 9: 
         _selectedDrawerItem = index;
-        Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const AccessPage()));   
+        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>  AccessPage()));   
         break;
       case 10: 
         _selectedDrawerItem = index;
