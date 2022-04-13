e275059faf21b50c7545ec906ee7d8b3d3a54ee3
Oscar-CR <krz.oficial@gmail.com>
2022-04-12 20:41:41 -0500

    prueba de funcionalidad de la api

--- lib/model/manual.dart
+++ lib/model/manual.dart
@@ -1,14 +1,14 @@
   import 'dart:convert';
 
-  List<Post> postFromJson(String str) =>
-      List<Post>.from(json.decode(str).map((x) => Post.fromMap(x)));
-
+List<Post> postFromJson(String str) =>
+    List<Post>.from(json.decode(str).map((x) => Post.fromMap(x)));
+    
   class Post {
     Post({
       required this.id,
       required this.name,
       required this.file,
-      required this.img,
+      /* required this.img, */
       required this.created_at,
       required this.updated_at,
     
@@ -17,7 +17,7 @@
     int id;
     String name;
     String file;
-    String img;
+   /*  String img; */
     String created_at;
     String updated_at;
     
@@ -25,7 +25,7 @@
           id: json["id"],
           name: json["name"],
           file: json["file"],
-          img: json["img"],
+         /*  img: json["img"], */
           created_at: json["created_at"],
           updated_at: json["updated_at"],
         );
--- lib/views/manual/manual_page.dart
+++ lib/views/manual/manual_page.dart
@@ -1,9 +1,24 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:intranet_movil/model/manual.dart';
-import 'package:intranet_movil/networking/manual.dart';
+/* import 'package:intranet_movil/networking/manual.dart'; */
+import 'package:http/http.dart' as http;
 
+Future<List<Post>> getManuals() async{
+  final response =
+      await http.get(Uri.parse('https://intranet.kaths.com.mx/api/manuals'));
+
+    if (response.statusCode == 200) {
+ 
+      final parsed = json.decode(response.body).cast<Map<String, dynamic>>();
+      return parsed.map<Post>((json) => Post.fromMap(json)).toList();
+
+    } else {
+      throw Exception('Failed to load manual');
+    }
+  }
 void main() => runApp(ManualPage());
 
 class ManualPage extends StatefulWidget {
@@ -12,16 +27,16 @@ class ManualPage extends StatefulWidget {
 }
 
 class _MyAppState extends State<ManualPage> {
-  late Future<List<Post>> futurePost;
+  late Future<List<Post>> futurePost ;
 
   @override
   void initState() {
     super.initState();
-    futurePost = fetchManual();
-    log('data: $futurePost');
-  }
-
+    futurePost = getManuals();
   
+  /*   log('data: $futurePost'); */
+   /*  print(futurePost); */
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,42 +49,21 @@ class _MyAppState extends State<ManualPage> {
         appBar: AppBar(
           title: Text('Fetch Data Example'),
         ),
-        body: FutureBuilder<List<Post>>(
+        body: FutureBuilder(
           future: futurePost,
           builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (_, index) => Container(
-                  child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    padding: EdgeInsets.all(20.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xff97FFFF),
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "${snapshot.data![index].name}",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Text("${snapshot.data![index].name}"),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            } else {
-              return Center(child: CircularProgressIndicator());
+            if(snapshot.hasData){
+              print(snapshot);
+              return Text("Hola");
+            }else if(snapshot.hasError){
+              print(snapshot.error);
+              return Text("Adios");
             }
-          },
+
+            return const Center(
+              child:  CircularProgressIndicator(),
+            );
+          } ,
         ),
       ),
     );
