786c3e6f56d74a2312fbbefc59c339f41e987edb
Oscar-CR <krz.oficial@gmail.com>
2022-04-21 16:05:09 -0500

    agregado de funcionalidad de busqueda

--- lib/views/directory/directory_page.dart
+++ lib/views/directory/directory_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intranet_movil/model/directory.dart';
 import 'package:intranet_movil/services/api_directory.dart';
@@ -14,11 +16,14 @@ class DirectoryPage extends StatefulWidget {
 
 class _HomeState extends State<DirectoryPage> {
   late List<DirectoryModel>? _directoryModel = [];
-  
+  late List<DirectoryModel>? _directoryModelSearch = [];
+  final _debouncer = Debouncer();
+
   @override
   void initState() {
     super.initState();
     _getData();
+    _directoryModelSearch = _directoryModel;
   }
 
   void _getData() async {
@@ -39,11 +44,45 @@ class _HomeState extends State<DirectoryPage> {
             )
           : Column(
             children: [
-              Text("Hola"),
+              TextField(
+              textInputAction: TextInputAction.search,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                  ),
+                ),
+                suffixIcon: InkWell(
+                  child: Icon(Icons.search),
+                ),
+                contentPadding: EdgeInsets.all(15.0),
+                hintText: 'Search ',
+              ),
+              onChanged: (string) {
+                _debouncer.run(() {
+                  setState(() {
+                    _directoryModelSearch = _directoryModel
+                        ?.where(
+                          (u) => (u.name.toLowerCase().contains(
+                                string.toLowerCase(),
+                              )),
+                        )
+                        .toList();
+                  });
+                });
+              },
+            ),
               Expanded(
                 child: ListView.builder(
                   padding: const EdgeInsets.only(left:16, right: 16),
-                  itemCount: _directoryModel!.length,
+                  itemCount: _directoryModelSearch!.length,
                   itemBuilder: (context, index) {
                     return Card(
                       shape: RoundedRectangleBorder(
@@ -54,11 +93,11 @@ class _HomeState extends State<DirectoryPage> {
                         onTap: (){
                           UserCardAlertDialog.showFullDialog(
                             context,
-                            _directoryModel![index].name + " " +_directoryModel![index].lastname,
-                            _directoryModel![index].email,
-                            ApiIntranetConstans.baseUrl + _directoryModel![0].photo, 
-                            _directoryModel![index].department,
-                            _directoryModel![index].position );
+                            _directoryModelSearch![index].name + " " +_directoryModelSearch![index].lastname,
+                            _directoryModelSearch![index].email,
+                            ApiIntranetConstans.baseUrl + _directoryModelSearch![0].photo, 
+                            _directoryModelSearch![index].department,
+                            _directoryModelSearch![index].position );
                         },
                         child: Row(
                         children: [
@@ -68,7 +107,7 @@ class _HomeState extends State<DirectoryPage> {
                               width: 50,
                               height: 50,
                               child:CircleAvatar(
-                              backgroundImage: NetworkImage(ApiIntranetConstans.baseUrl + _directoryModel![0].photo.toString()),
+                              backgroundImage: NetworkImage(ApiIntranetConstans.baseUrl + _directoryModelSearch![0].photo.toString()),
                               /*  backgroundImage: NetworkImage(ApiIntranetConstans.baseUrl + _directoryModel![index].photo.toString()), */                          ),
                             )    
                           ),
@@ -76,7 +115,7 @@ class _HomeState extends State<DirectoryPage> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text(
-                                  _directoryModel![index].name + " " +_directoryModel![index].lastname,
+                                  _directoryModelSearch![index].name + " " +_directoryModelSearch![index].lastname,
                                   maxLines: 1,
                                   overflow: TextOverflow.ellipsis,
                                   style: const TextStyle(fontSize: 16.00,fontWeight: FontWeight.bold,),
@@ -85,7 +124,7 @@ class _HomeState extends State<DirectoryPage> {
                                 padding: EdgeInsets.only(top:8)
                               ),
                               Text(
-                                  _directoryModel![index].position,
+                                  _directoryModelSearch![index].position,
                                   maxLines: 1,
                                   overflow: TextOverflow.ellipsis,
                                   style: const TextStyle(fontSize: 12.00,)
@@ -101,6 +140,22 @@ class _HomeState extends State<DirectoryPage> {
               ),
             ],
           ) 
+    );
+  }
+}
+
+class Debouncer {
+  int? milliseconds;
+  VoidCallback? action;
+  Timer? timer;
+
+  run(VoidCallback action) {
+    if (null != timer) {
+      timer!.cancel();
+    }
+    timer = Timer(
+      Duration(milliseconds: Duration.millisecondsPerSecond),
+      action,
     );
   }
 }
