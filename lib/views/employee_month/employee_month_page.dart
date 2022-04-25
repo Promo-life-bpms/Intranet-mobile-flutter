cb79fe174ddd5fbbdca06d2a0addfd3fa3ef7454
Sarai Cab <saracabrera.myg@gmail.com>
2022-04-25 12:29:39 -0500

    configEmpleados

--- lib/views/employee_month/employee_month_page.dart
+++ lib/views/employee_month/employee_month_page.dart
@@ -31,7 +31,7 @@ class _HomeState extends State<EmployeeMonthPage> {
     return Scaffold(
       drawer: const NavigationDrawerWidget(),
       appBar: AppBar(
-        title: const Text('Comunicados'),
+        title: const Text("Empleado del Mes"),
       ),
       body: _monthEmployeeModel == null || _monthEmployeeModel!.isEmpty
           ? const Center(
@@ -46,6 +46,27 @@ class _HomeState extends State<EmployeeMonthPage> {
                     padding: const EdgeInsets.only(bottom: 20.0),
                     child: Column(
                       children: [
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 20.0),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 12, top: 0, right: 12, bottom: 0),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: Text(
+                              "¡Gracias por tu compromiso y dedicación!",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Color.fromRGBO(96, 125, 139, 1),
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 20.0),
+                        ),
                         SizedBox(
                           width: double.infinity,
                           height: 320.0,
@@ -70,6 +91,27 @@ class _HomeState extends State<EmployeeMonthPage> {
                         const Padding(
                           padding: EdgeInsets.only(bottom: 20.0),
                         ),
+                        Text(
+                          _monthEmployeeModel![index].position,
+                          style: const TextStyle(
+                            fontSize: 20.00,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(bottom: 20.0),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const <Widget>[
+                            Icon(
+                              Icons.star,
+                              color: Color.fromARGB(255, 28, 47, 56),
+                              size: 24,
+                            ),
+                          ],
+                        ),
+                        Container(
+                            child: Text(_monthEmployeeModel![index].star)),
                       ],
                     ),
                   ),
