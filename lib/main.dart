e201553aa236fe76ffe738db817b88391d053134
Oscar-CR <krz.oficial@gmail.com>
2022-05-05 15:59:52 -0500

    depuracion de codigo y mejora de ui al validar sesion

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:intranet_movil/model/user_model.dart';
-import 'package:intranet_movil/services/api_user.dart';
 import 'package:intranet_movil/services/auth.dart';
 import 'package:intranet_movil/views/auth/login_page.dart';
 import 'package:intranet_movil/views/home/home_page.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-/* void main() => runApp( const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: "presentacion",
-      routes: {
-        "presentacion": (_) => const Presentacion(),
-        "inicio": (_) => const Login(),
-      },
-    );
-  }
-} */
-
-
-/* 
-void main() {
-  runApp(
-      ChangeNotifierProvider(
-        create: (BuildContext context) => AuthProvider(),
-        child: const MyApp(),
-      )
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return 
-     MaterialApp(
-      title: 'Login',
-      home:  Scaffold(
-        body: Center(
-            child: Consumer<AuthProvider>(
-              builder: (context, auth, child) {
-                switch (auth.isAuthenticated) {
-                  case true:
-                    return  const HomePage();
-                  default:
-                    return  const LoginForm();
-                }
-              },
-            )
-        ),
-      )
-    );
-  }
-}
- */
 
 void main() {
   runApp(
@@ -95,7 +38,7 @@ class _HomeState extends State<MyApp> {
     _token = prefs.getString('token');
       Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return 
@@ -103,7 +46,12 @@ class _HomeState extends State<MyApp> {
       title: 'Login',
       home:  Scaffold(
         body:
-        _token == null || _token!.isEmpty || _token == ""
+        _token == ""?
+        const Center(
+          child: CircularProgressIndicator(),
+        )
+        :
+        _token == null || _token!.isEmpty 
         ?Center(
             child: Consumer<AuthProvider>(
               builder: (context, auth, child) {
