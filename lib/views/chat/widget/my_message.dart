62c095ed8e57c0be046f9e62d711c103fc0fa11b
Oscar-CR <krz.oficial@gmail.com>
2022-07-21 10:51:02 -0500

    agregado de selector de texto en los chats

--- lib/views/chat/widget/my_message.dart
+++ lib/views/chat/widget/my_message.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:intranet_movil/services/internet.dart';
 import 'package:intranet_movil/utils/constants.dart';
 
 class MyMessageItem extends StatefulWidget {
-  const MyMessageItem({Key? key, required this.conversation,required this.created}) : super(key: key);
+  const MyMessageItem(
+      {Key? key, required this.conversation, required this.created})
+      : super(key: key);
 
   final String conversation;
   final String created;
@@ -14,25 +18,29 @@ class MyMessageItem extends StatefulWidget {
 class _MyMessageItemState extends State<MyMessageItem> {
   @override
   Widget build(BuildContext context) {
-    return widget.created == "no data"?
-     const Padding(padding: EdgeInsets.zero)
-    :
-    Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Flexible(
-          child: Container(
-              decoration: const BoxDecoration(
-                color: ColorIntranetConstants.primaryColorNormal,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(12.0),
+    return widget.created == "no data"
+        ? const Padding(padding: EdgeInsets.zero)
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Flexible(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: ColorIntranetConstants.primaryColorNormal,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12.0),
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(12),
+                  child: SelectableLinkify(
+                    onOpen: (link) =>  LaunchToInternet.launchURL(widget.conversation),
+                    text: widget.conversation,
+                    style: const TextStyle(color: Colors.white),
+                    textAlign: TextAlign.justify,
+                  ),
                 ),
-              ),
-              padding: const EdgeInsets.all(12),
-              child: Text(widget.conversation, textAlign: TextAlign.justify,
-                  style: const TextStyle(color: Colors.white))),
-        )
-      ],
-    );
+              )
+            ],
+          );
   }
 }
--- lib/views/chat/widget/other_message.dart
+++ lib/views/chat/widget/other_message.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
+import 'package:intranet_movil/services/internet.dart';
 import 'package:intranet_movil/utils/constants.dart';
 
 class OtherMessageItem extends StatefulWidget {
-  const OtherMessageItem({Key? key, required this.conversation, required this.created}): super(key: key);
+  const OtherMessageItem(
+      {Key? key, required this.conversation, required this.created})
+      : super(key: key);
 
   final String conversation;
-    final String created;
+  final String created;
 
   @override
   State<OtherMessageItem> createState() => _OtherMessageItemState();
@@ -14,26 +18,29 @@ class OtherMessageItem extends StatefulWidget {
 class _OtherMessageItemState extends State<OtherMessageItem> {
   @override
   Widget build(BuildContext context) {
-    return 
-    widget.created == "no data"?
-     const Padding(padding: EdgeInsets.zero)
-    :
-    Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Flexible(
-          child: Container(
-              decoration: const BoxDecoration(
-                color: ColorIntranetConstants.backgroundColorDark,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(12.0),
+    return widget.created == "no data"
+        ? const Padding(padding: EdgeInsets.zero)
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: ColorIntranetConstants.backgroundColorDark,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12.0),
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(12),
+                  child: SelectableLinkify(
+                    onOpen: (link) => LaunchToInternet.launchURL(widget.conversation),
+                    text: widget.conversation,
+                    style: const TextStyle(color: Colors.black),
+                    textAlign: TextAlign.justify,
+                  ),
                 ),
-              ),
-              padding: const EdgeInsets.all(12),
-              child: Text(widget.conversation, textAlign: TextAlign.justify,
-                  style: const TextStyle(color: Colors.black))),
-        )
-      ],
-    );
+              )
+            ],
+          );
   }
 }
